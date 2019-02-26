660416b846ffb6410f171483dc0f7cff340ddaf1
carru <carru400@gmail.com>
2019-02-25 19:10:28 -0800

    Add edit category name functionality. Missing styling

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 import { Category } from '../../models/Category';
 import { Topic } from 'src/app/models/Topic';
 import { TopicService } from 'src/app/services/topic.service';
@@ -18,10 +18,12 @@ export class CategoryComponent implements OnInit {
   topics:Topic[];
   presetColours: string[];
   topicBackground: string;
+  isEditMode: boolean;
 
   constructor(private topicService:TopicService, private categoryService:CategoryService, private socketService:SocketService) { }
 
   ngOnInit() {
+    this.isEditMode = false;
     this.topics = this.category.topics;
     this.onColorPickerChange(this.category.colour);
     this.presetColours = ['#90a4ae', '#ff8a65', '#ffd54f', '#aed581', '#4db6ac', '#4fc3f7', '#7986cb', '#ba68c8', '#e57373'];
@@ -50,11 +52,30 @@ export class CategoryComponent implements OnInit {
     });
   }
 
-  onPlusOne(category:Category) {
+  onClickPlusOne() {
     if (typeof this.category.points == 'undefined') {
       this.category.points = 0;
     }
     this.category.points++;
     this.saveCategory(this.category);
+  }
+
+  @ViewChild("name") nameInput: ElementRef;
+  onClickEdit() {
+    this.toggleEditMode();
+    if (this.isEditMode) {
+      setTimeout(() => this.nameInput.nativeElement.focus(), 0);
+    }
+  }
+
+  toggleEditMode() {
+    this.isEditMode = !this.isEditMode;
+  }
+
+  onSubmit() {
+    this.categoryService.updateCategory(this.category).subscribe( () => {
+      this.toggleEditMode();
+      this.socketService.notifyDataHasChanged();
+    });
   }
 }
